0a4aa73dcfe3b33353be33b469f1723f2ed7fded
Richard Myatt <lydgate77@hotmail.co.uk>
2018-05-18 15:46:01 +0100

    rearangement of sass files and styling partially completed.  More work needed on responsive design and styling of preview text.

--- js/markdown.js
+++ js/markdown.js
@@ -1,11 +1,36 @@
 new Vue({
   el: "#app",
   data: {
-    input: ""
+    input: "",
+    labelsEnabled: true
+  },
+  methods: {
+    handleResize: function() {
+      if(window.innerWidth < 700) {
+        this.labelsEnabled = false;
+      } else {
+        this.labelsEnabled = true;
+      }
+    }
   },
   computed: {
     preview: function() {
       return marked(this.input);
     }
+  },
+  created: function() {
+    window.addEventListener('resize', this.handleResize);
   }
 });
+
+/*function myFunction(x) {
+    if (x.matches) { // If media query matches
+        document.body.style.backgroundColor = "yellow";
+    } else {
+        document.body.style.backgroundColor = "pink";
+    }
+}
+
+var x = window.matchMedia("(max-width: 700px)");
+myFunction(x); // Call listener function at run time
+x.addListener(myFunction); // Attach listener function on state changes*/
