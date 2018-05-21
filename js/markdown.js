aefd44f5d4e871058641d5228b94f780ce616219
Richard Myatt <lydgate77@hotmail.co.uk>
2018-05-21 22:13:28 +0100

    Application altered and working but without syntax highlighting

--- js/markdown.js
+++ js/markdown.js
@@ -1,6 +1,12 @@
-var initialText = "Markdown - Demo\n========================\n\nThis is a markdown previewer.  Markdown is a simple text format which is easy to read and write.  It is commonly used by sites such as Git Hub.  This previewer allows you to preview Git Hub style markdown.  To learn more about how to write markdown go to [Mastering Markdown](https:\/\/guides.github.com/features/mastering-markdown/).\n\nYou can use this demo to type any markdown you choose and it will automatically be previewed.  You can:\n\n1. Type markdown in the left pane.\n2. See the live preview in the right pane.\n3. Clear this left pane using the 'Clear' button.\n\nPlease note that this previewer is not capable of showing syntax highlighting in code snippets at this time. [clear everything](?text=)";
+/* Author: Richard Myatt
+     Date: 21 May 2018
 
-var main = new Vue({
+     A markdown previewer.  Prepared as an exercise for freecodecamp.
+*/
+
+var initialText = "Welcome\n======\n\nMarkdown is a simple text format which allows the writer to prepare a document which can be converted into other formats such as HTML.  In comparison to writing a document in HTML the format is relatively simple and straightforward and also provides the benefit that it can be easily read before conversion.  This form of documentation is used by sites like git hub where the Readme.md is prepared in markdown.  For more information on markdown see [Mastering Markdown](https://guides.github.com/features/mastering-markdown/).\n\nIllustration\n------------\n\nAs a quick illustration unordered lists can be written as follows:\n\n- this is an unordeered list\n- ordered lists are just as easy\n\t1. This is an indented ordered list\n\t2. and so on.\n\t\nCode snippets, blockquotes and images are also easily include.  For example a code snippet takes the form:\n\n``` python\n\na = 10\nprint(a)\n\n```\n\nA block quote attributed to Albert Einstein:\n\n> I am by heritage a Jew, by citizenship a Swiss, and by makeup a human being, and only a human being, without any special attachment to any state or national entity whatsoever.\n\nAnd finally an image.\n\n![Elephant](https://upload.wikimedia.org/wikipedia/commons/thumb/3/37/African_Bush_Elephant.jpg/220px-African_Bush_Elephant.jpg  'African Elephant')\n\nThis markdown previewer provides a live, instant, preview of any markdown entered.  If you would like to try it out please either alter the existing text or press the \'Clear\' button.  You can easily return to this screen by pressing the \'Welcome\' button.";
+
+new Vue({
   el: "#app",
   data: {
     input: initialText,
@@ -28,18 +34,5 @@ var main = new Vue({
   },
   mounted: function() {
     this.handleResize();
-    //this.input = initialText;
   }
 });
-
-window.onload = function() {
-  var clear = setInterval(function()
-  {
-      var currentPage = window.location.href;
-      if ("?text=" === currentPage.substr(-6))
-      {
-          main.input = "";
-          clearInterval(clear);
-      }
-  }, 200);
-};
---
+++ miscelaneous/welcome.md
@@ -0,0 +1,33 @@
+Welcome
+======
+
+Markdown is a simple text format which allows the writer to prepare a document which can be converted into other formats such as HTML.  In comparison to writing a document in HTML the format is relatively simple and straightforward and also provides the benefit that it can be easily read before conversion.  This form of documentation is used by sites like git hub where the Readme.md is prepared in markdown.  For more information on markdown see [Mastering Markdown](https://guides.github.com/features/mastering-markdown/).
+
+Illustration
+------------
+
+As a quick illustration unordered lists can be written as follows:
+
+- this is an unordeered list
+- ordered lists are just as easy
+	1. This is an indented ordered list
+	2. and so on.
+	
+Code snippets, blockquotes and images are also easily include.  For example a code snippet takes the form:
+
+``` python
+
+a = 10
+print(a)
+
+```
+
+A block quote attributed to Albert Einstein:
+
+> I am by heritage a Jew, by citizenship a Swiss, and by makeup a human being, and only a human being, without any special attachment to any state or national entity whatsoever.
+
+And finally an image.
+
+![Elephant](https://upload.wikimedia.org/wikipedia/commons/thumb/3/37/African_Bush_Elephant.jpg/220px-African_Bush_Elephant.jpg  'African Elephant')
+
+This markdown previewer provides a live, instant, preview of any markdown entered.  If you would like to try it out please either alter the existing text or press the 'Clear' button.  You can easily return to this screen by pressing the 'Welcome' button.
